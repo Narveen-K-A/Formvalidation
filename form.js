4e746a979c60877d76f39c217d31ef668ac2b59f
Narveen-K-A <114560275+Narveen-K-A@users.noreply.github.com>
2022-10-17 17:59:31 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,16 @@
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+};
