e1ea46f2a0d3eee7f03b31e4c9169c139e916137
Narveen K A <narveen@techversantinfotech.com>
2022-10-19 18:06:31 +0530

    File Commit

--- form.js
+++ form.js
@@ -11,13 +11,10 @@ function validateform(){
    	var joinmonth=document.forms['form']['joinmonth'];
    	var joinweek=document.forms['form']['joinweek'];
 	var jointime=document.forms['form']['jointime'];
-	var program=document.forms['form']['program'];
 	var quantity=document.forms['form']['quantity'];
-	var favcolor=document.forms['form']['favcolor'];
-   	var vol=document.forms['form']['vol'];
 	var gsearch=document.forms['form']['gsearch'];
 	var homepage=document.forms['form']['homepage'];
-	var photo=document.forms['form']['photo'];
+	var file=document.forms['form']['file'];
 	
 	nameValidate(fname);
 	emailValidate(email);
@@ -31,13 +28,10 @@ function validateform(){
 	joinmonthValidate(joinmonth);
 	joinweekValidate(joinweek);
 	jointimeValidate(jointime);
-	programValidate(program);
 	quantityValidate(quantity);
-	favcolorValidate(favcolor);
-	volValidate(vol);
 	gsearchValidate(gsearch);
 	homepageValidate(homepage);
-	photoValidate(photo);
+	fileValidate(file);
 	return false;
 }
 
@@ -49,3 +43,211 @@ function successValidate(id){
   	document.getElementById(id).innerHTML="";
 }
 
+function nameValidate(fname){
+	if(fname.value==""){
+		errorValidate('error1');
+		fname.style.border="1px solid #ff0000";  
+     		return false;
+	}
+	else{
+		successValidate('error1');
+		fname.style.border="1px solid #00ff00";  
+     		return true;
+	}
+}
+
+function emailValidate(email){
+   	if(email.value==""){
+     		errorValidate('error2');
+     		email.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error2');
+      		email.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function phoneValidate(phone){
+   	if(phone.value==""){
+     		errorValidate('error3');
+     		phone.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else if(phone.value.length<10){
+     		errorValidate('error3');
+     		phone.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error3');
+      		phone.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function passwordValidate(password){
+   	if(password.value==""){
+     		errorValidate('error4');
+     		password.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error4');
+      		password.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function birthdaytimeValidate(birthdaytime){
+   	if(birthdaytime.value==""){
+     		errorValidate('error5');
+     		birthdaytime.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error5');
+		birthdaytime.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function stateValidate(state){
+   	if(state.value==""){
+     		errorValidate('error6');
+		state.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error6');
+		state.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function addressValidate(address){
+   	if(address.value==""){
+     		errorValidate('error7');
+		address.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error7');
+		address.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function genderValidate(gender){
+   	if(gender.value==""){
+     		errorValidate('error8');  
+     		return false;
+   	}
+	else{
+      		successValidate('error8');  
+     		return true;
+   	}
+}
+
+function curdateValidate(curdate){
+   	if(curdate.value==""){
+     		errorValidate('error9');
+		curdate.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error9');
+		curdate.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function joinmonthValidate(joinmonth){
+   	if(joinmonth.value==""){
+     		errorValidate('error10');
+		joinmonth.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error10');
+		joinmonth.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function joinweekValidate(joinweek){
+   	if(joinweek.value==""){
+     		errorValidate('error11');
+		joinweek.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error11');
+		joinweek.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function jointimeValidate(jointime){
+   	if(jointime.value==""){
+     		errorValidate('error12');
+		jointime.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error12');
+		jointime.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function quantityValidate(quantity){
+   	if(quantity.value==""){
+     		errorValidate('error13');
+		quantity.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error13');
+		quantity.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function gsearchValidate(gsearch){
+   	if(gsearch.value==""){
+     		errorValidate('error14');
+		gsearch.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error14');
+		gsearch.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function homepageValidate(homepage){
+   	if(homepage.value==""){
+     		errorValidate('error15');
+		homepage.style.border="1px solid #ff0000";  
+     		return false;
+   	}
+	else{
+      		successValidate('error15');
+		homepage.style.border="1px solid #00ff00";  
+     		return true;
+   	}
+}
+
+function fileValidate(file){
+   	if(file.value==""){
+     		errorValidate('error16');  
+     		return false;
+   	}
+	else{
+      		successValidate('error16');  
+     		return true;
+   	}
+}
